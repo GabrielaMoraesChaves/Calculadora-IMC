359ed66d07499f2786fde7ef7ab71ee4d827e037
GabrielaMoraesChaves <gm.120994@gmail.com>
2024-09-28 15:49:02 -0300

    mostrar mensagem preencha os campos

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -3,7 +3,9 @@ package com.comunidadedevspace.imc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,17 +19,32 @@ class MainActivity : AppCompatActivity() {
         val btnCalcular = findViewById<Button>(R.id.btn_calcular)
 
         btnCalcular.setOnClickListener{
-            val peso: Float = editPeso.text.toString().toFloat()
-            println("Você Clicou!" + peso)
 
-            val altura: Float = editAltura.text.toString().toFloat()
-            println("Você Clicou!" + altura)
+
+            val pesoStr: String = editPeso.text.toString()
+            val alturaStr: String = editAltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+
+                Snackbar.make(
+                    editPeso,
+                    "Preencha Todos os Campos",
+                    Snackbar.LENGTH_SHORT
+                )
+                    .show()
+
+            }else{
+
+            val peso = pesoStr.toFloat()
+            val altura = alturaStr.toFloat()
 
             val alturaQ2 = altura * altura
             val resultado = peso / alturaQ2
-            println("Ação Do Botão" + resultado)
-        }
 
+            println("Acao do botao" + resultado)
+
+            }
         }
 
     }
+}
